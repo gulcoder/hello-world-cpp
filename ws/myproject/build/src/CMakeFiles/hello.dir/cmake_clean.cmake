67c619082eb0bab80165060c457fabe622c3001f
Gul Baran <gulbaran243@gmail.com>
2025-03-14 10:37:10 +0100

    initial commit with C++ Hello World project

---
+++ ws/myproject/build/src/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/gulbaran/hello-world-cpp/ws/myproject/bin/hello"
+  "/Users/gulbaran/hello-world-cpp/ws/myproject/bin/hello.pdb"
+  "CMakeFiles/hello.dir/main.cpp.o"
+  "CMakeFiles/hello.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
